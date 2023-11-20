66e27d56bb7c00cbd751504bbcf23d2c94772e07
Nguyen Tuan Anh <anh.nguyentuan3@nashtechglobal.com>
2023-11-20 11:23:47 +0700

    handle logic assignment

--- classes/pickup.py
+++ classes/pickup.py
@@ -19,10 +19,19 @@ class PickUp(Customer):
         self.leave_day = leave_day
         self.info = {}
         self.bill = {}
+        self.bills = []
 
     def get_arrival_day(self):
-        get_arrival_time =  self.get_customer_information()[ARRIVAL_TIME_TEXT].strip()
-        return datetime.datetime.strptime(get_arrival_time, "%Y-%m-%d %H:%M")
+        try:
+            get_arrival_time =  self.get_customer_information()[ARRIVAL_TIME_TEXT].strip()
+            arrival_day = datetime.datetime.strptime(get_arrival_time, "%Y-%m-%d %H:%M")
+            if(0 <= arrival_day.hour < 8):
+                raise Exception("OOPs, arrival time went wrong")
+        except Exception as e:
+            print(e)
+            exit()
+        else:
+            return arrival_day
 
     def get_leave_day(self):
         return datetime.datetime.strptime(self.leave_day, "%Y-%m-%d %H:%M")
@@ -39,39 +48,69 @@ class PickUp(Customer):
         new_day = (day + datetime.timedelta(days=1)).replace(hour=8, minute=0)
         return [day_time, night_time, new_day] 
 
-    def get_fee_each_day(self, start_day, data):
+    def get_fee_each_day(self, start_day, end_day, data):
         [day_time, night_time, new_day] = self.split_periods_day(start_day)
         self.bill['date'] = start_day.strftime("%Y-%m-%d")
         self.bill['dayOfWeek'] = DAY_OF_WEEK[start_day.weekday()]
         parking_regulation = data[self.bill['dayOfWeek']]
+
         for park in parking_regulation:
             temp_str = park['start'] + ' - ' + park['end']
-            
+            park['pricePerHour'] = round(park['pricePerHour'], 2)
             if park['start'] == '08:00' and park['end'] == '16:59':
-               sub_time = round((day_time - start_day).seconds/3600)
-               self.bill[temp_str] = [
-                {
-                    "time": park['maxStayInHours'],
-                    "price_per_hour": park['pricePerHour']
-                },
-                {
-                    "time": sub_time - int(park['maxStayInHours']),
-                    "price_per_hour": park['pricePerHour'] * 2
-                }
-               ]
+                if (start_day < end_day and end_day < day_time):
+                    sub_time = round((end_day - start_day).seconds/3600)
+                    time_remain = sub_time - int(park['maxStayInHours'])
+                elif(day_time < start_day) :
+                    sub_time = 0
+                    time_remain = 0
+                else:
+                    sub_time = round((day_time - start_day).seconds/3600)
+                    time_remain = sub_time - int(park['maxStayInHours'])
+
+                self.bill[temp_str] = [
+                    {
+                        "time": sub_time if sub_time <= 2 else park['maxStayInHours'],
+                        "price_per_hour": sub_time if sub_time == 0 else park['pricePerHour'],
+                        "str_price_per_hour": '' if sub_time == 0 else f'${"{0:.2f}".format(park["pricePerHour"])}/h' 
+                    },
+                    {
+                        "time": 0 if (sub_time == 0 or time_remain <= 0 ) else time_remain,
+                        "price_per_hour": 0 if time_remain <= 0 else park['pricePerHour'] * 2,
+                        "str_price_per_hour": '' if time_remain <= 0 else f'${"{0:.2f}".format(park["pricePerHour"])}/h * 2' 
+                    }
+                ]
             elif park['start'] == '17:00' and park['end'] == '23:59':
-                sub_time = (night_time - day_time).seconds//3600
+                if( day_time < start_day < end_day and end_day < night_time):
+                    sub_time = round((end_day - start_day).seconds/3600)
+                elif(day_time < start_day):
+                    sub_time = round((night_time - start_day).seconds/3600)
+                elif(day_time < end_day < night_time):
+                    sub_time = round((end_day - day_time).seconds/3600) 
+                elif(end_day < day_time):
+                    sub_time = 0     
+                else:
+                    sub_time = round((night_time - day_time).seconds/3600)
+
                 self.bill[temp_str] = [
                 {
                     "time": sub_time,
-                    "price_per_hour": park['pricePerHour']
+                    "price_per_hour": park['pricePerHour'] if sub_time != 0 else 0,
+                    "str_price_per_hour": f'${"{0:.2f}".format(park["pricePerHour"])}/h' if sub_time != 0 else ''
                 }
                ]
             else:
+                sub_time = 1
+                if(end_day < day_time or day_time < end_day < night_time):
+                    sub_time = 0
+                elif(night_time < start_day < end_day):
+                    sub_time = round((end_day - start_day)/3600)
+
                 self.bill[temp_str] = [
                 {
-                    "time": 'N/A',
-                    "price_per_hour": park['pricePerHour']
+                    "time": 'N/A' if sub_time != 0 else 0,
+                    "price_per_hour": park['pricePerHour'] if sub_time != 0 else 0,
+                    "str_price_per_hour": f'${"{0:.2f}".format(park["pricePerHour"])}' if sub_time != 0 else ''
                 }
                ]
         
@@ -90,13 +129,42 @@ class PickUp(Customer):
         self.bill['sub_total'] = sub_total
         return self.bill
 
+    def get_bills_parking(self, data):
+        start_day = self.get_arrival_day()
+        end_day = self.get_leave_day()
+        range_date = self.get_range_date()
+        for index, day in enumerate(range_date):
+            day = datetime.datetime.strptime(day, "%Y-%m-%d")
+            next_day = (day + datetime.timedelta(days=1)).replace(hour=8, minute=0)
+            self.bill = {}
+            if index == 0 and len(range_date) > 1:
+                self.get_fee_each_day(start_day=start_day, end_day= next_day, data=data)
+                self.calculate_sub_total_each_day()
+            elif index == 0 and len(range_date) == 1:
+                self.get_fee_each_day(start_day=start_day, end_day= end_day, data=data)
+                self.calculate_sub_total_each_day()
+                self.bills.append(self.bill)
+                return self.bills
+            elif (index == len(range_date) - 1) :
+                previous_time = end_day.replace(hour=8, minute=0)
+                self.get_fee_each_day(start_day=previous_time, end_day= end_day, data=data)
+                self.calculate_sub_total_each_day()
+            else:
+                day = day.replace(hour=8, minute=0)
+                self.get_fee_each_day(start_day=day, end_day= next_day, data=data)
+                self.calculate_sub_total_each_day()
+            self.bills.append(self.bill)
+        return self.bills
+
     def calculate_fee_parking(self, data):
         try:
             if(self.is_customer_information_exist() == False):
                 raise Exception('Your car identity not found')
         except Exception:
             print("Your car identity not found")
+            exit()
         else:
-            self.get_fee_each_day(self.get_arrival_day(), data)
-            print(self.calculate_sub_total_each_day())
-            return 
+            self.get_bills_parking(data=data)
+            total = sum([bill['sub_total'] for bill in self.bills])
+            return [self.bills, total]
+        
--- options.py
+++ options.py
@@ -1,21 +1,29 @@
 from classes.park import Park
 from classes.pickup import PickUp
+from create_excel import create_excel
 from datetime import datetime
+
 
 def park_option():
     arrival_time = datetime.now().replace(second=0, microsecond=0).strftime("%Y-%m-%d %H:%M")
     try:
+        if(0 <= datetime.strptime(arrival_time, '%Y-%m-%d %H:%M').hour < 8):
+            raise Exception("CÚT!!!!")
         car_identity = input('Enter your car identity: ').strip()
         frequent_parking_number = input('Enter your frequent parking number (if so): ')
         frequent_parking_number = None if frequent_parking_number == '' else int(frequent_parking_number)
     except ValueError:
         print("OOPS, something went wrong!")
+    except Exception as e:
+        print(e)
     else:
         cus = Park(car_identity=car_identity, arrival_time=arrival_time, frequent_parking_number=frequent_parking_number)
         cus.save_customer_information()
 
 def pickup_option(data):
     enter_car_identity = input("Please enter your car identity: ") 
-    leave_day = datetime.now().replace(second=0, microsecond=0).strftime("%Y-%m-%d %H:%M")
+    leave_day = datetime.now().replace(day=15, hour= 19, minute=30, second=0, microsecond=0).strftime("%Y-%m-%d %H:%M")
     pick_up = PickUp(car_identity=enter_car_identity, leave_day=leave_day)
-    pick_up.calculate_fee_parking(data)
+    [bills, total] = pick_up.calculate_fee_parking(data)
+    print(f'Your total fee parking: {total}')
+    create_excel(car_identity=pick_up.car_identity,leave_day=leave_day, bills=bills)
